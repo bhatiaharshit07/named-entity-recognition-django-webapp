93f676b11195330ead0236efc0ea8eaa2af5fc56
bhatiahashit07 <bhatiahashit07@gmail.com>
2021-03-29 21:05:33 +0530

    updated settings.py

--- finalProject/settings.py
+++ finalProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'uas-^b!1@c7$+uxll@o(4-_12(2ob#oz!&u4p9ipbiel+h!of+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
+ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com','127.0.0.1']
 
 
 # Application definition
