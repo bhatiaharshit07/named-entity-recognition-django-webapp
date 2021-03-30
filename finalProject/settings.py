210ec56fb5ff43065ed99b76cb66a6522f5fc46c
bhatiahashit07 <bhatiahashit07@gmail.com>
2021-03-30 16:10:34 +0530

    updated settings.py

--- finalProject/settings.py
+++ finalProject/settings.py
@@ -133,3 +133,19 @@ STATICFILES_DIRS = (
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Configure Django App for Heroku.
+'''
+git add .
+git commit -m "updated settings.py"
+git push heroku master
+'''
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
