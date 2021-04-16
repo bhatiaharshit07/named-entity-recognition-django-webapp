b39ef74d6232ad06c5381ca4b0e71f8bb4ad1d37
bhatiaharshit07 <bhatiaharshit07@gmail.com>
2021-04-17 00:40:25 +0530

    changed form method from get-> post

--- NER/views.py
+++ NER/views.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpResponse
+
 from django.shortcuts import render
 import spacy
 from spacy import displacy
@@ -8,17 +8,12 @@ def home(request):
 
 def result(request):
 
-    input_sentence = request.GET['input_text']
+    input_sentence = request.POST.get('input_text')
 
     nlp = spacy.load("en_core_web_md")
     text = nlp(input_sentence)
     html = displacy.render([text], style="ent")
-    '''
-    ex = [{"text": "But Google is starting from behind.",
-       "ents": [{"start": 4, "end": 10, "label": "ORG"}],
-       "title": None}]
-    html = displacy.render(ex, style="ent", manual=True)
-    '''
+
     #return HttpResponse(html[308:-7])
     #return render(request, "result.html", {'ans':text, 'ans2':nerList, 'content':html[344:-25]})
     return render(request, "result.html", {'content':html})
